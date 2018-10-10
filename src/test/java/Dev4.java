54b01dd6989c3063024993dd4e6f33c6d367cb8f
pangshanxing <pangshanxing@edianzu.com>
2018-10-10 17:34:05 +0800

    ceshi4

---
+++ src/test/java/Dev4.java
@@ -0,0 +1,11 @@
+public class Dev4 {
+
+    private String dev4;
+
+    @Override
+    public String toString() {
+        return "Dev4{" +
+                "dev4='" + dev4 + '\'' +
+                '}';
+    }
+}
